9e006a71f5eba35890922c16aea7c97f9499d1ad
Brian Warner <warner@lothar.com>
2018-11-14 16:52:24 -0800

    add VatState, a counter incremented by deliver()
    Each VatID gets a separate VatState

--- runtime/src/vat.rs
+++ runtime/src/vat.rs
@@ -1,6 +1,6 @@
 
 //use parity_codec::Encode;
-use srml_support::{StorageValue, dispatch::Result};
+use srml_support::{StorageValue, StorageMap, dispatch::Result};
 //use runtime_primitives::traits::Hash;
 use runtime_io::print;
 use balances;
@@ -11,10 +11,18 @@ pub trait Trait: balances::Trait {}
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 
-    fn deliver(_origin, vatID: u64) -> Result {
+    fn deliver(_origin, vat_id: u64) -> Result {
 			//let sender = ensure_signed(origin)?;
-      print("delivery to vatID");
-      print(vatID);
+      print("delivery to vat_id");
+      print(vat_id);
+      //print("key:"); print(<VatCounter<T>>::key());
+      if <VatState<T>>::exists(vat_id) {
+          <VatState<T>>::mutate(vat_id, |state| *state += 1);
+          print(" incremented vat state to");
+          print(<VatState<T>>::get(vat_id));
+      } else {
+          print("no such vat_id");
+      }
 			//let payment = Self::payment().ok_or("Must have payment amount set")?;
 			//<balances::Module<T>>::decrease_free_balance(&sender, payment)?;
 			//<balances::Module<T>>::increase_free_balance_creating(&sender, <Pot<T>>::take());
@@ -23,8 +31,10 @@ decl_module! {
 
 		fn create_vat(_origin) -> Result {
 			<VatCounter<T>>::mutate(|c| *c += 1);
+      let vat_id = Self::vatCounter();
 			print("vatCounter is");
-			print(Self::vatCounter());
+			print(vat_id);
+      <VatState<T>>::insert(vat_id, 0);
       // format! is not available in nostd
       //print(format!("vatCounter is {}", Self::vatCounter()));
 			Ok(())
@@ -35,6 +45,7 @@ decl_module! {
 decl_storage! {
 	trait Store for Module<T: Trait> as Demo {
 		VatCounter get(vatCounter) config(): u64;
+		VatState get(vatState): map u64 => u64;
 		Unused get(unused) config(): T::Balance;
 	}
 }
